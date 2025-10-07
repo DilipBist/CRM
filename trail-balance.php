2048fd0c2e6115fdec7210a9bffda3af542fe7fa
Dilip-Bist <dilipbist879@gmail.com>
2025-10-07 15:11:35 +0545

    created the trail-balance page

---
+++ trail-balance.php
@@ -0,0 +1,55 @@
+<?php
+$pageTitle = 'Trail Balance';
+include 'inc/header.php'
+?>
+
+<section class="admin_container d-flex">
+
+
+    <!-- SIDE BAR  -->
+    <?php
+    $currentPage = 'trailBalance';
+    include 'inc/side-bar.php';
+    ?>
+    <!-- SIDE BAR ENDS  -->
+
+    <div class="admin_right_content ms-auto">
+
+        <!-- TOP PROFILE HEADER  -->
+        <div class="admin_top_header d-flex align-items-center gap-3 justify-content-between">
+            <div class="chart_bread_crump d-flex align-items-center gap-3">
+                <div class="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
+                        <path d="M9 1.5C7.035 1.5 5.4375 3.0975 5.4375 5.0625C5.4375 6.99 6.945 8.55 8.91 8.6175C8.97 8.61 9.03 8.61 9.075 8.6175C9.09 8.6175 9.0975 8.6175 9.1125 8.6175C9.12 8.6175 9.12 8.6175 9.1275 8.6175C11.0475 8.55 12.555 6.99 12.5625 5.0625C12.5625 3.0975 10.965 1.5 9 1.5Z" fill="#141414" />
+                        <path d="M12.8107 10.6127C10.7182 9.21766 7.3057 9.21766 5.1982 10.6127C4.2457 11.2502 3.7207 12.1127 3.7207 13.0352C3.7207 13.9577 4.2457 14.8127 5.1907 15.4427C6.2407 16.1477 7.6207 16.5002 9.0007 16.5002C10.3807 16.5002 11.7607 16.1477 12.8107 15.4427C13.7557 14.8052 14.2807 13.9502 14.2807 13.0202C14.2732 12.0977 13.7557 11.2427 12.8107 10.6127Z" fill="#141414" />
+                    </svg>
+                </div>
+
+                <div class="bread_crump_content d-flex align-items-center gap-2">
+                    <a href="opening-trails.php">Account Report </a>
+                    <span>/</span>
+                    <p> Trail Balance</p>
+                </div>
+            </div>
+
+            <div class="admin_profile_container d-flex align-items-center gap-3">
+
+                <?php include 'components/profile.php' ?>
+
+
+                <?php include 'components/notification.php' ?>
+            </div>
+        </div>
+        <!-- TOP PROFILE HEADER ENDS  -->
+
+
+    </div>
+
+    </div>
+
+
+
+</section>
+
+
+<?php include 'inc/footer.php' ?>
