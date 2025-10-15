6af7249e3d3fd6957f6b8c294a008cf559b867c9
Dilip-Bist <dilipbist879@gmail.com>
2025-10-15 17:21:06 +0545

    upated the geo fence page and created the add geo fence page

---
+++ add-geo-fence.php
@@ -0,0 +1,81 @@
+<?php
+$pageTitle = 'Add Geo Fence';
+include 'inc/header.php'
+?>
+
+
+<!-- delet popup component  -->
+<?php include 'components/delete-popup.php' ?>
+<!-- delet popup component ends -->
+
+<section class="admin_container d-flex">
+
+
+    <?php
+    $currentPage = 'geoFence';
+    include 'inc/side-bar.php';
+    ?>
+
+    <div class="admin_right_content ms-auto user_list">
+        <!-- top profile header  -->
+        <div class="admin_top_header d-flex align-items-center gap-3 justify-content-between">
+            <div class="chart_bread_crump d-flex align-items-center gap-3">
+                <div class="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
+                        <path d="M9 1.5C7.035 1.5 5.4375 3.0975 5.4375 5.0625C5.4375 6.99 6.945 8.55 8.91 8.6175C8.97 8.61 9.03 8.61 9.075 8.6175C9.09 8.6175 9.0975 8.6175 9.1125 8.6175C9.12 8.6175 9.12 8.6175 9.1275 8.6175C11.0475 8.55 12.555 6.99 12.5625 5.0625C12.5625 3.0975 10.965 1.5 9 1.5Z" fill="#141414" />
+                        <path d="M12.8097 10.6127C10.7172 9.21766 7.30473 9.21766 5.19723 10.6127C4.24473 11.2502 3.71973 12.1127 3.71973 13.0352C3.71973 13.9577 4.24473 14.8127 5.18973 15.4427C6.23973 16.1477 7.61973 16.5002 8.99973 16.5002C10.3797 16.5002 11.7597 16.1477 12.8097 15.4427C13.7547 14.8052 14.2797 13.9502 14.2797 13.0202C14.2722 12.0977 13.7547 11.2427 12.8097 10.6127Z" fill="#141414" />
+                    </svg>
+                </div>
+
+                <div class="bread_crump_content d-flex align-items-center gap-2">
+                    <a href="employee.php">HRMS </a>
+                    <span>/</span>
+                    <p>Geo Fences</p>
+                </div>
+            </div>
+
+            <div class="admin_profile_container d-flex align-items-center gap-3">
+                <?php include 'components/profile.php' ?>
+                <?php include 'components/notification.php' ?>
+            </div>
+        </div>
+        <!-- top profile header  -->
+
+
+
+        <div class="admin_list_heading d-flex align-items-center flex-wrap gap-3 justify-content-between outer_padding">
+            <div class="left_heading">
+                <h3>Geo Fences List</h3>
+                <p>You can view the Geo Fences list accordingly.</p>
+            </div>
+
+            <div class="right_fil_div d-flex align-items-center gap-2 flex-wrap justify-content-md-end">
+                <div class="search">
+                    <input type="text" placeholder="Search" name="search">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                        <path d="M7.66634 14.0007C11.1641 14.0007 13.9997 11.1651 13.9997 7.66732C13.9997 4.16951 11.1641 1.33398 7.66634 1.33398C4.16854 1.33398 1.33301 4.16951 1.33301 7.66732C1.33301 11.1651 4.16854 14.0007 7.66634 14.0007Z" stroke="#141414" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M14.6663 14.6673L13.333 13.334" stroke="#141414" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                </div>
+
+                <button class="add_acc_group ">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                        <path d="M9.99935 18.3346C14.5827 18.3346 18.3327 14.5846 18.3327 10.0013C18.3327 5.41797 14.5827 1.66797 9.99935 1.66797C5.41602 1.66797 1.66602 5.41797 1.66602 10.0013C1.66602 14.5846 5.41602 18.3346 9.99935 18.3346Z" stroke="#F5F5F5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M6.66602 10H13.3327" stroke="#F5F5F5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M10 13.3346V6.66797" stroke="#F5F5F5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                    Add Geo Fence
+                </button>
+            </div>
+        </div>
+
+
+        <div class="pl_pr">
+
+        </div>
+    </div>
+</section>
+
+
+
+<?php include 'inc/footer.php' ?>
